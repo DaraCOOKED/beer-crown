0ef9d23af9f9f4cb8940c8f0c97518518168f744
kakadacancer-code <kakadacancer@gmail.com>
2026-06-19 12:55:54 +0700

    feat/schedule static done

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,8 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  css: ['./app/assets/css/main.css'],
+  css: ['./app/assets/css/main.css',
+        '@fortawesome/fontawesome-free/css/all.min.css'],
   vite: {
     plugins: [
       tailwindcss(),
