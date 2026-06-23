f4181ac9e267fd5a25061481ed918b906c14f2f9
DaraCOOKED <dara.somnag020@gmail.com>
2026-06-23 08:20:23 +0700

    added Github base URL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,23 +4,24 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
 
-  css: ['./app/assets/css/main.css',
-        '@fortawesome/fontawesome-free/css/all.min.css'],
-
-
+  css: [
+    "./app/assets/css/main.css",
+    "@fortawesome/fontawesome-free/css/all.min.css",
+  ],
 
   vite: {
     plugins: [tailwindcss()],
   },
 
   app: {
-  head: {
-    link: [
-      {
-        rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Poppins:wght@400;500;600;700;800&display=swap'
-      }
-    ]
-  }
-}
+    baseURL: "/beer-crown/",
+    head: {
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Poppins:wght@400;500;600;700;800&display=swap",
+        },
+      ],
+    },
+  },
 });
